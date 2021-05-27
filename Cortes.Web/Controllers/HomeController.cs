71d5e6bd58da775dc29e920aafd4e37a8a10529d
Felipe <felipe-mattioli98@hotmail.com>
2021-05-26 22:35:39 -0300

    Implementações

--- Cortes.Web/Controllers/HomeController.cs
+++ Cortes.Web/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Cortes.Repositorio.Interfaces.IUsuarioRepositorio;
 using Cortes.Repositorio.Repositorios.UsuarioRepositorio;
 using Cortes.Web.Models;
+using Cortes.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
@@ -11,27 +12,36 @@ namespace Cortes.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Login()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View();
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Registro()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [HttpPost]
+        public IActionResult Registro(RegistroViewModel model)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (ModelState.IsValid)
+            {
+                return View();
+            }
+            return View(model);
         }
+
     }
 }
---
+++ Cortes.Web/ViewModels/RegistroViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cortes.Web.ViewModels
+{
+    public class RegistroViewModel
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [EmailAddress(ErrorMessage = "Email inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigátorio")]
+        [StringLength(40, ErrorMessage = "Use menos caracteres")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+        [Required(ErrorMessage = "O campo {0} é obrigátorio")]
+        [StringLength(40, ErrorMessage = "Use menos caracteres")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirme sua senha")]
+        [Compare("Senha", ErrorMessage = "As senhas não são iguais")]
+        public string SenhaConfirmada { get; set; }
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string Telefone { get; set; }
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string Endereco { get; set; }
+    }
+}
